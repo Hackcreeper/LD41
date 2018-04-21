1878a8f044422ade273eed12898a065276275c6d
Jordan Kniest <mail@jkniest.de>
2018-04-21 16:26:26 +0200

    Wheels and rotation

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -2,9 +2,11 @@
 
 public class Car : MonoBehaviour
 {
-    private float _speed = 1000f;
-    private float _rotationSpeed = 80f;
-    
+    [SerializeField] private float _speed = 1000f;
+    [SerializeField] private float _rotationSpeed = 80f;
+
+    [SerializeField] private Transform[] _wheels;
+
     private Rigidbody _rigidbody;
     private Vector3 _velocity;
 
@@ -12,15 +14,22 @@ public class Car : MonoBehaviour
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
-    
+
     private void Update()
     {
         _velocity = transform.forward * (Input.GetAxis("Vertical") * _speed);
 
         if (_velocity.magnitude > 0f)
         {
-            var rotation = new Vector3(0, transform.localEulerAngles.y + Input.GetAxis("Horizontal") * _rotationSpeed * Time.deltaTime, 0);
+            var rotation = new Vector3(0,
+                transform.localEulerAngles.y + Input.GetAxis("Horizontal") * _rotationSpeed * Time.deltaTime, 0);
             transform.localEulerAngles = rotation;
+
+            // Rotate the wheels
+            foreach (var wheel in _wheels)
+            {
+                wheel.transform.Rotate(new Vector3(0, 1, 0), _velocity.normalized.magnitude * 10);
+            }
         }
     }
 
