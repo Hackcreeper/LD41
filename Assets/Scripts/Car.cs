81945f40fec004b4751c821a565175d1304bc78e
Jordan Kniest <mail@jkniest.de>
2018-04-21 15:59:36 +0200

    Added car movement (WASD)

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    private float _speed = 1000f;
+    private float _rotationSpeed = 80f;
+    
+    private Rigidbody _rigidbody;
+    private Vector3 _velocity;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+    
+    private void Update()
+    {
+        _velocity = transform.forward * (Input.GetAxis("Vertical") * _speed);
+
+        if (_velocity.magnitude > 0f)
+        {
+            var rotation = new Vector3(0, transform.localEulerAngles.y + Input.GetAxis("Horizontal") * _rotationSpeed * Time.deltaTime, 0);
+            transform.localEulerAngles = rotation;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = _velocity * Time.fixedDeltaTime;
+    }
+}
