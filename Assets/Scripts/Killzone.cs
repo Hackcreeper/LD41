b393e524765f5d95a4a82c1ce377d57db195245e
Jordan Kniest <mail@jkniest.de>
2018-04-22 02:33:48 +0200

    The saws are now killing zombies

--- Assets/Scripts/Killzone.cs
+++ Assets/Scripts/Killzone.cs
@@ -3,6 +3,7 @@
 public class Killzone : MonoBehaviour
 {
     [SerializeField] private bool _front;
+    [SerializeField] private bool _static;
     [SerializeField] private Car _player;
 
     private void OnTriggerStay(Collider other)
@@ -10,7 +11,7 @@ public class Killzone : MonoBehaviour
         if (!other.CompareTag("Enemy")) return;
         if (other.GetComponent<Zombie>().IsDead()) return;
 
-        if (_front && _player.GetPower() > 0)
+        if ((_front && _player.GetPower() > 0) || _static)
         {
             ZombieManager.Instance.Kill(other.transform);
             return;
