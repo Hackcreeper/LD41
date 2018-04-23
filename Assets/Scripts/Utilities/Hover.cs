960a5a1a205e58855606ffade3e3fe64860bc8cd
Jordan Kniest <mail@jkniest.de>
2018-04-23 15:00:36 +0200

    Collectables are now rotating and hovering

---
+++ Assets/Scripts/Utilities/Hover.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+namespace Utilities
+{
+    public class Hover : MonoBehaviour
+    {
+        [SerializeField] private float _factor = 0.008f;
+        [SerializeField] private float _speed = 2;
+
+        private Vector3 _basePosition;
+
+        private void Start()
+        {
+            _basePosition = transform.localPosition;
+        }
+
+        private void Update()
+        {
+            var y = Mathf.Lerp(transform.localPosition.y, _basePosition.y + Mathf.Sin(Time.time * _speed), _factor);
+            
+            transform.localPosition = new Vector3(
+                _basePosition.x,
+                y,
+                _basePosition.z
+            );
+        }
+    }
+}
